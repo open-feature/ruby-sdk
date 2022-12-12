dafc176b0021d2d03f5874fff2113ed436a72d20
Jose Colella <jose.colella@gusto.com>
2022-12-12 13:51:40 -0800

    chore(client): fix rubocop warnings
    Signed-off-by: Jose Colella <jose.colella@gusto.com>

--- lib/openfeature/sdk/client.rb
+++ lib/openfeature/sdk/client.rb
@@ -8,10 +8,8 @@ module OpenFeature
     # TODO: Write documentation
     #
     class Client
-
-      RESULT_TYPE = %i(boolean string number object).freeze
-      SUFFIXES = %i(value details)
-
+      RESULT_TYPE = %i[boolean string number object].freeze
+      SUFFIXES = %i[value details].freeze
 
       attr_reader :metadata
 
@@ -24,18 +22,19 @@ module OpenFeature
         @hooks = Concurrent::Array.new([])
       end
 
-
       RESULT_TYPE.each do |result_type|
         SUFFIXES.each do |suffix|
           class_eval <<-RUBY, __FILE__, __LINE__ + 1
+            # def fetch_boolean_details(flag_key:, default_value:, evaluation_context: nil)
+            #   result = @provider.fetch_boolean_value(flag_key: flag_key, default_value: default_value, evaluation_context: evaluation_context)
+            # end
             def fetch_#{result_type}_#{suffix}(flag_key:, default_value:, evaluation_context: nil)
               result = @provider.fetch_#{result_type}_value(flag_key: flag_key, default_value: default_value, evaluation_context: evaluation_context)
-              #{'result.value' if suffix == :value}
+              #{"result.value" if suffix == :value}
             end
           RUBY
         end
       end
-
     end
   end
 end

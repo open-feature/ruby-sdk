1073723f28db541f7b247eba43e3ce9e294e7fa9
Jose Colella <jose.colella@gusto.com>
2022-12-12 13:39:06 -0800

    refactor(client): remove need for extra evaluate_flag_details and value  methods
    Signed-off-by: Jose Colella <jose.colella@gusto.com>

--- lib/openfeature/sdk/client.rb
+++ lib/openfeature/sdk/client.rb
@@ -29,36 +29,13 @@ module OpenFeature
         SUFFIXES.each do |suffix|
           class_eval <<-RUBY, __FILE__, __LINE__ + 1
             def fetch_#{result_type}_#{suffix}(flag_key:, default_value:, evaluation_context: nil)
-              evaluate_flag_#{suffix}(flag_type: #{result_type.inspect}, flag_key: flag_key, default_value: default_value,
-                evaluation_context: evaluation_context)
+              result = @provider.fetch_#{result_type}_value(flag_key: flag_key, default_value: default_value, evaluation_context: evaluation_context)
+              #{'result.value' if suffix == :value}
             end
           RUBY
         end
       end
 
-      def evaluate_flag_details(flag_type:, flag_key:, default_value:, evaluation_context: nil)
-        case flag_type
-        when :boolean
-          @provider.fetch_boolean_value(flag_key: flag_key, default_value: default_value,
-                                        evaluation_context: evaluation_context)
-        when :number
-          @provider.fetch_number_value(flag_key: flag_key, default_value: default_value,
-                                       evaluation_context: evaluation_context)
-        when :string
-          @provider.fetch_string_value(flag_key: flag_key, default_value: default_value,
-                                       evaluation_context: evaluation_context)
-        when :object
-          @provider.fetch_object_value(flag_key: flag_key, default_value: default_value,
-                                       evaluation_context: evaluation_context)
-        else
-          raise ArgumentError.new("Unsupported flag_type: #{flag_type}")
-        end
-      end
-
-      def evaluate_flag_value(flag_type:, flag_key:, default_value:, evaluation_context: nil)
-        details = evaluate_flag_details(flag_key: flag_key, flag_type: flag_type, default_value: default_value, evaluation_context: evaluation_context)
-        details.value
-      end
     end
   end
 end

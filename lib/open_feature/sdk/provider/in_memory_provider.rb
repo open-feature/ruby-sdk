aa243d0f9f0d62d2d21fd790bba8cf2af3de9284
Max VelDink <maxveldink@gmail.com>
2024-02-28 13:31:06 -0500

    feat: Adds InMemoryProvider for storing flags in process
    Signed-off-by: Max VelDink <maxveldink@gmail.com>

---
+++ lib/open_feature/sdk/provider/in_memory_provider.rb
@@ -0,0 +1,54 @@
+module OpenFeature
+  module SDK
+    module Provider
+      # TODO: Add evaluation context support
+      class InMemoryProvider
+        NAME = "In-memory Provider"
+
+        def initialize(flags = {})
+          @metadata = Metadata.new(name: NAME).freeze
+          @flags = flags
+        end
+
+        def add_flag(flag_key:, value:)
+          flags[flag_key] = value
+          # TODO: Emit PROVIDER_CONFIGURATION_CHANGED event once events are implemented
+        end
+
+        def fetch_boolean_value(flag_key:, default_value:, evaluation_context: nil)
+          fetch_value(allowed_classes: [TrueClass, FalseClass], flag_key:, default_value:, evaluation_context:)
+        end
+
+        def fetch_string_value(flag_key:, default_value:, evaluation_context: nil)
+          fetch_value(allowed_classes: [String], flag_key:, default_value:, evaluation_context:)
+        end
+
+        def fetch_number_value(flag_key:, default_value:, evaluation_context: nil)
+          fetch_value(allowed_classes: [Integer, Float], flag_key:, default_value:, evaluation_context:)
+        end
+
+        def fetch_object_value(flag_key:, default_value:, evaluation_context: nil)
+          fetch_value(allowed_classes: [Array, Hash], flag_key:, default_value:, evaluation_context:)
+        end
+
+        private
+
+        attr_reader :flags
+
+        def fetch_value(allowed_classes:, flag_key:, default_value:, evaluation_context:)
+          value = flags[flag_key]
+
+          if value.nil?
+            return ResolutionDetails.new(value: default_value, error_code: ErrorCode::FLAG_NOT_FOUND, reason: Reason::ERROR)
+          end
+
+          if allowed_classes.include?(value.class)
+            ResolutionDetails.new(value:, reason: Reason::STATIC)
+          else
+            ResolutionDetails.new(value: default_value, error_code: ErrorCode::TYPE_MISMATCH, reason: Reason::ERROR)
+          end
+        end
+      end
+    end
+  end
+end

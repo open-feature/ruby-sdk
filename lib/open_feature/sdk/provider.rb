25680a40b0955ee66da256f23f7078655754a4b6
Max VelDink <maxveldink@gmail.com>
2024-03-06 07:29:25 -0500

    feat: adds `InMemoryProvider` (#102)
    Signed-off-by: Max VelDink <maxveldink@gmail.com>

--- lib/open_feature/sdk/provider.rb
+++ lib/open_feature/sdk/provider.rb
@@ -2,6 +2,7 @@ require_relative "provider/error_code"
 require_relative "provider/reason"
 require_relative "provider/resolution_details"
 require_relative "provider/no_op_provider"
+require_relative "provider/in_memory_provider"
 
 module OpenFeature
   module SDK
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,8 @@ require "open_feature/sdk"
 
 require "markly"
 
+require "debug"
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
@@ -16,6 +18,8 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.filter_run_when_matching :focus
 
   # ie for GitHub Actions
   # see https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables

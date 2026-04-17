f13a1813837a646102fccc5dd4256f0fe4e2031f
Cristian Marta <cristian@caffe-lento.com>
2026-04-17 03:36:27 -0300

    fix: freeze ClientMetadata to satisfy spec req 4.2.2.1
    Signed-off-by: Cristian Marta <cristian@caffe-lento.com>

--- lib/open_feature/sdk/client_metadata.rb
+++ lib/open_feature/sdk/client_metadata.rb
@@ -4,6 +4,11 @@ module OpenFeature
   module SDK
     ClientMetadata = Struct.new(:domain, keyword_init: true) do
       alias_method :name, :domain
+
+      def initialize(domain: nil)
+        super(domain: domain&.dup&.freeze)
+        freeze
+      end
     end
   end
 end
